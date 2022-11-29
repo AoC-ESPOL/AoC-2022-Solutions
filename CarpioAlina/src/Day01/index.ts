340bdcad1473045717e90ce8564740675db8facd
alicarpio <agcarpio@espol.edu.ec>
2022-11-29 15:24:48 -0500

    add CarpioAlina folder

---
+++ CarpioAlina/src/Day01/index.ts
@@ -0,0 +1,5 @@
+import * as PartOne from "./PartOne"
+import * as PartTwo from "./PartTwo"
+
+export const partOne = PartOne.default;
+export const partTwo = PartTwo.default;
---
+++ CarpioAlina/src/main.ts
@@ -0,0 +1,8 @@
+import {
+    partOne,
+    partTwo
+} from "./Day01"
+
+
+console.log(`Part one solution: ${partOne()}`)
+console.log(`Part two solution: ${partTwo()}`)
