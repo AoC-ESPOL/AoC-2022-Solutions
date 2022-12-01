97f67d51b54d28da758344f6d7a9baebc388b582
wgcotera <wgcotera@espol.edu.ec>
2022-12-01 03:03:22 -0500

    rearrange code

--- CoteraWashington/src/main/java/day_01/PartOne.java
+++ CoteraWashington/src/main/java/day_01/PartOne.java
@@ -1,40 +1,14 @@
 package day_01;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import static day_01.common.sumOfCalories;
 
 public class PartOne {
-    public static void main(String[] args) throws IOException {
-        List<String> listOfCalories = createListOfCalories("src/main/resources/inputOne.txt");
-        List<Integer> sumOfCalories = createListOfSumCalories(listOfCalories);
-        System.out.println(sumOfCalories.stream().mapToInt(v -> v).max().getAsInt());
-    }
+    public static void main(String[] args) {
 
-    public static List<String> createListOfCalories(String fileName) throws IOException {
-        List<String> list = new ArrayList<>();
-        BufferedReader br = new BufferedReader(new FileReader(fileName));
-        String line;
-        while ((line = br.readLine()) != null) {
-            list.add(line);
-        }
-        br.close();
-        return list;
-    }
+        System.out.println(sumOfCalories.stream()
+                .mapToInt(v -> v)
+                .max()
+                .getAsInt());
 
-    public static List<Integer> createListOfSumCalories(List<String> listOfCalories) {
-        List<Integer> sumCalories = new ArrayList<>();
-        int sumCal = 0;
-        for (String c : listOfCalories) {
-            if (c.equals("")) {
-                sumCalories.add(sumCal);
-                sumCal = 0;
-            } else {
-                sumCal += Integer.parseInt(c);
-            }
-        }
-        return sumCalories;
     }
 }
--- CoteraWashington/src/main/java/day_01/PartTwo.java
+++ CoteraWashington/src/main/java/day_01/PartTwo.java
@@ -1,18 +1,18 @@
 package day_01;
 
-import java.io.IOException;
-import java.util.List;
+import java.util.stream.Collectors;
 
-import static day_01.PartOne.createListOfCalories;
-import static day_01.PartOne.createListOfSumCalories;
+import static day_01.common.sumOfCalories;
 
 public class PartTwo {
-    public static void main(String[] args) throws IOException {
-        List<String> listOfCalories = createListOfCalories("src/main/resources/inputOne.txt");
-        List<Integer> sumOfCalories = createListOfSumCalories(listOfCalories);
-        sumOfCalories.sort((o1, o2) -> o2 - o1);
-        int sum = sumOfCalories.get(0) + sumOfCalories.get(1) + sumOfCalories.get(2);
-        System.out.println(sum);
+    public static void main(String[] args) {
+
+        System.out.println(sumOfCalories.stream()
+                .sorted((o1, o2) -> o2 - o1)
+                .collect(Collectors.toList())
+                .subList(0, 3).stream()
+                .mapToInt(v -> v)
+                .sum());
 
     }
 }
