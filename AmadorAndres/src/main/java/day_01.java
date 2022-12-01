61e5a526373021d29505186f719a5a6e0d347c8d
amadoran <afamador@espol.edu.ec>
2022-12-01 02:04:51 -0500

    Solucion p1d1

--- AmadorAndres/src/main/java/day_01.java
+++ AmadorAndres/src/main/java/day_01.java
@@ -1,8 +1,31 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class day_01 {
 
 	public static void main(String[] args) {
-		System.out.println("Hola mundo");
+		int max = 0;
+		
+		try (BufferedReader br = new BufferedReader(new FileReader("src/main/resources/input/input1.txt"))){
+			int count = 0;
+			String linea;
+			while((linea = br.readLine()) != null) {
+				if (linea.isEmpty()) {
+					if(count > max) {
+						max = count;
+					}
+					count = 0;
+				} else {
+					int cantidad = Integer.parseInt(linea);
+					count += cantidad;
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println(max);
 	}
-
+	
 }
