115fffa13235ef3b856d1b500bbe8f99f39afeb6
alicarpio <agcarpio@espol.edu.ec>
2022-12-01 23:46:33 -0500

    Part 2

--- CarpioAlina/src/Day01/PartTwo.ts
+++ CarpioAlina/src/Day01/PartTwo.ts
@@ -1,3 +1,8 @@
+import { getCaloriesPerElf} from '../common'
+
+let result = getCaloriesPerElf();
 export default function solution() {
-    
+  result.sort((a,b) => b - a);
+  
+  return result[0] + result[1] + result[2]
 }
--- CarpioAlina/src/common.ts
+++ CarpioAlina/src/common.ts
@@ -5,3 +5,22 @@ export function getInput(inputName: string): string {
     encoding: 'utf-8',
   })
 }
+
+export function getCaloriesPerElf(){
+  const input = getInput('01')
+  let caloriesPerElf = input.split('\n\n')
+  let caloriesSplit = []
+  let result: number[] = []
+  let totalCalories;
+  for (let c of caloriesPerElf) {
+    caloriesSplit.push(c.split('\n').map(Number))
+  }
+  caloriesSplit.forEach((arr) => {
+     totalCalories = arr.reduce((accum, current) => {
+      return accum + current
+    }, 0)
+    result.push(totalCalories)
+   
+  })
+  return result;
+}
