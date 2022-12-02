6ec1d4cc6a920ccd07081def76396da74b99fa41
amadoran <afamador@espol.edu.ec>
2022-12-01 19:20:11 -0500

    parte2 day01

--- AmadorAndres/src/main/java/day_01.java
+++ AmadorAndres/src/main/java/day_01.java
@@ -1,10 +1,15 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 
 public class day_01 {
 
 	public static void main(String[] args) {
+		//parte1
 		int max = 0;
 		
 		try (BufferedReader br = new BufferedReader(new FileReader("src/main/resources/input/input1.txt"))){
@@ -26,6 +31,29 @@ public class day_01 {
 		}
 		
 		System.out.println(max);
+		
+		//parte2
+		List<Integer> totales = new LinkedList<>();
+		try (BufferedReader br = new BufferedReader(new FileReader("src/main/resources/input/input1.txt"))){
+			int count = 0;
+			String linea;
+			while((linea = br.readLine()) != null) {
+				if (linea.isEmpty()) {
+					totales.add(count);
+					count = 0;
+				} else {
+					int cantidad = Integer.parseInt(linea);
+					count += cantidad;
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		Collections.sort(totales);
+		Collections.reverse(totales);
+		Iterator<Integer> totalesIt = totales.iterator();
+		System.out.println(totalesIt.next() + totalesIt.next() + totalesIt.next());
 	}
 	
 }
