adf417848907eb9a046a6a78b941da604bba08ce
alicarpio <agcarpio@espol.edu.ec>
2022-12-01 23:46:33 -0500

    Part 2

--- CarpioAlina/src/Day01/PartOne.ts
+++ CarpioAlina/src/Day01/PartOne.ts
@@ -1,27 +1,13 @@
 import { getInput } from '../common'
+import { getCaloriesPerElf} from '../common'
 
-const totalCalories = 0
-const current = 0
 
 export default function solution() {
-  const input = getInput('01')
-  let caloriesPerElf = input.split('\n\n')
-  let caloriesSplit = []
-  let result: number[] = []
-  let totalCalories;
-  for (let c of caloriesPerElf) {
-    caloriesSplit.push(c.split('\n').map(Number))
-  }
-  caloriesSplit.forEach((arr) => {
-     totalCalories = arr.reduce((accum, current) => {
-      return accum + current
-    }, 0)
-    result.push(totalCalories)
-   
-  })
+  const result = getCaloriesPerElf();
  
-  
   const max = result.reduce((a, b) => Math.max(a, b), -Infinity)
 
   return max
 }
+
+
