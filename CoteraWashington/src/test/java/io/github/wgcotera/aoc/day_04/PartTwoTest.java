c1131426d4442cc35c1e59bddbc55a07e9a4a434
wgcotera <wgcotera@espol.edu.ec>
2022-12-04 00:55:48 -0500

    wgcotera day_04

---
+++ CoteraWashington/src/test/java/io/github/wgcotera/aoc/day_04/PartTwoTest.java
@@ -0,0 +1,22 @@
+package io.github.wgcotera.aoc.day_04;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PartTwoTest {
+
+    @Test
+    void solution() {
+
+        var partTwo = new PartTwo();
+        var solution = partTwo.solution("""
+                                        2-4,6-8
+                                        2-3,4-5
+                                        5-7,7-9
+                                        2-8,3-7
+                                        6-6,4-6
+                                        2-6,4-8""");
+        assertEquals(solution, 4);
+    }
+}
