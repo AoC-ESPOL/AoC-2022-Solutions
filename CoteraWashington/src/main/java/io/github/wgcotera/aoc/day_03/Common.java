016c95daa0660c13e1fbd94fa159382fcda44dbf
wgcotera <wgcotera@espol.edu.ec>
2022-12-03 22:11:46 -0500

    refactor code

--- CoteraWashington/src/main/java/io/github/wgcotera/aoc/day_03/Common.java
+++ CoteraWashington/src/main/java/io/github/wgcotera/aoc/day_03/Common.java
@@ -8,6 +8,8 @@ public class Common {
     //    Lowercase item types a through z have priorities 1 through 26.
     //    Uppercase item types A through Z have priorities 27 through 52.
 
+    public static Map<String, Integer> mapOfLetterPriority = createMapOfLetterPriority();
+
     public static Map<String, Integer> createMapOfLetterPriority() {
         Map<String, Integer> map = new HashMap<>();
         for (int i = 1; i <= 26; i++) {
