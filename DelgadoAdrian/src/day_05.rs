28de1c20fef88e2d28571452339330d96976593f
Adrian Delgado <git@adrianjdelgado.com>
2022-12-05 02:29:12 -0500

    Day 5

--- DelgadoAdrian/src/day_05.rs
+++ DelgadoAdrian/src/day_05.rs
@@ -1,7 +1,101 @@
-pub fn part1(_input: &str) -> String {
-    todo!()
+use bstr::ByteSlice;
+use nom::{bytes::complete::tag, character::complete::u32, sequence::tuple, IResult};
+
+pub fn part1(input: &str) -> String {
+    let (crates, moves) = input.split_once("\n\n").unwrap();
+    let mut stacks = parse_crates(crates.as_bytes());
+
+    for (amount, from, to) in moves.lines().map(|line| parse_move(line).unwrap().1) {
+        let split_idx = stacks[from].len() - amount;
+        let mut extracted = stacks[from].split_off(split_idx);
+        extracted.reverse();
+        stacks[to].extend_from_slice(&extracted);
+    }
+
+    stacks
+        .into_iter()
+        .map(|mut stack| stack.pop().unwrap())
+        .collect()
 }
 
-pub fn part2(_input: &str) -> String {
-    todo!()
+pub fn part2(input: &str) -> String {
+    let (crates, moves) = input.split_once("\n\n").unwrap();
+    let mut stacks = parse_crates(crates.as_bytes());
+
+    for (amount, from, to) in moves.lines().map(|line| parse_move(line).unwrap().1) {
+        let split_idx = stacks[from].len() - amount;
+        let extracted = stacks[from].split_off(split_idx);
+        // extracted.reverse();
+        stacks[to].extend_from_slice(&extracted);
+    }
+
+    stacks
+        .into_iter()
+        .map(|mut stack| stack.pop().unwrap())
+        .collect()
+}
+
+fn parse_crates(input: &[u8]) -> Vec<Vec<char>> {
+    const BLOCK_LEN: usize = "[_] ".len();
+
+    let width = input.find("\n").unwrap() + "\n".len();
+    let cols = width / BLOCK_LEN;
+    let rows = input.len() / width;
+
+    let mut stacks = vec![Vec::<char>::with_capacity(rows); cols];
+
+    for (col_idx, curr_stack) in stacks.iter_mut().enumerate().take(cols) {
+        for current_row in 1..=rows {
+            let col_position = BLOCK_LEN * col_idx;
+            let rows_before = rows - current_row;
+            let chars_in_rows_before = rows_before * width;
+            let current_position = chars_in_rows_before + col_position + "[".len();
+
+            let current_char = input[current_position] as char;
+
+            if current_char != ' ' {
+                curr_stack.push(current_char);
+            }
+        }
+    }
+
+    stacks
+}
+
+fn parse_move(input: &str) -> IResult<&str, (usize, usize, usize)> {
+    let (rest, (_, a, _, b, _, c)) =
+        tuple((tag("move "), u32, tag(" from "), u32, tag(" to "), u32))(input)?;
+    // Subtract 1 to use as an index
+    Ok((rest, (a as usize, b as usize - 1, c as usize - 1)))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{part1, part2};
+
+    const TEST_INPUT: &str = "    [D]    \n\
+[N] [C]    \n\
+[Z] [M] [P]\n\
+ 1   2   3 \n\
+\n\
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2";
+
+    #[test]
+    #[ignore]
+    fn part1_works() {
+        let output = "CMZ".to_string();
+
+        assert_eq!(part1(TEST_INPUT), output);
+    }
+
+    #[test]
+    #[ignore]
+    fn part2_works() {
+        let output = "MCD".to_string();
+
+        assert_eq!(part2(TEST_INPUT), output);
+    }
 }
