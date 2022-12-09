4b565ff69c7cc8d2187cdd76e49a2293a61b89cb
Adrian Delgado <git@adrianjdelgado.com>
2022-12-09 02:51:23 -0500

    Optimize

--- DelgadoAdrian/src/day_09.rs
+++ DelgadoAdrian/src/day_09.rs
@@ -1,163 +1,82 @@
-#![allow(unused)]
-
 use std::collections::HashSet;
 
-use bstr::ByteSlice;
-use itertools::Itertools;
-use ndarray::{array, s, Array2};
 use nom::{
-    branch::alt,
-    bytes::complete::{is_not, tag},
-    character::complete::{alpha1, digit1, i64, u32},
-    combinator::value,
-    sequence::{delimited, preceded, separated_pair, terminated, tuple},
-    IResult,
-};
-use petgraph::{
-    algo::{all_simple_paths, toposort},
-    prelude::*,
+    branch::alt, bytes::complete::tag, character::complete::u8, combinator::value,
+    sequence::separated_pair, IResult,
 };
 
 pub fn part1(input: &str) -> usize {
     let mut positions_tail = HashSet::new();
-    let mut head = [0_i64, 0];
-    let mut tail = [0_i64, 0];
+    let mut snake = [[0; 2]; 2]; // head 0, tail 1
+
     for (dir, n) in input.lines().map(|line| parse_line(line).unwrap().1) {
         for _ in 0..n {
-            match dir {
-                Dir::Up => {
-                    head[1] += 1;
-                }
-                Dir::Down => {
-                    head[1] -= 1;
-                }
-                Dir::Left => {
-                    head[0] -= 1;
-                }
-                Dir::Right => {
-                    head[0] += 1;
-                }
-            }
-            let dx = head[0] - tail[0];
-            let dy = head[1] - tail[1];
-            let direction = [dx.signum(), dy.signum()];
-            let too_far = dx * dx + dy * dy > 2;
-            if too_far {
-                match direction {
-                    [1, 0] => tail[0] += 1,
-                    [1, 1] => {
-                        tail[0] += 1;
-                        tail[1] += 1
-                    }
-                    [0, 1] => tail[1] += 1,
-                    [-1, 1] => {
-                        tail[0] -= 1;
-                        tail[1] += 1;
-                    }
-                    [-1, 0] => tail[0] -= 1,
-                    [-1, -1] => {
-                        tail[0] -= 1;
-                        tail[1] -= 1;
-                    }
-                    [0, -1] => tail[1] -= 1,
-                    [1, -1] => {
-                        tail[0] += 1;
-                        tail[1] -= 1;
-                    }
-                    _ => (),
-                }
-            }
-            positions_tail.insert(tail);
+            let [ref mut head, ref mut tail] = snake;
+
+            add_inplace(head, dir);
+            update_snake_mut(tail, *head);
+
+            positions_tail.insert(*tail);
         }
     }
+
     positions_tail.len()
 }
 
 pub fn part2(input: &str) -> usize {
     let mut positions_tail = HashSet::new();
-    let mut snake = [[0_i64, 0_i64]; 10]; // head 0, tail 9
+    let mut snake = [[0; 2]; 10]; // head 0, tail 9
     for (dir, n) in input.lines().map(|line| parse_line(line).unwrap().1) {
         for _ in 0..n {
-            match dir {
-                Dir::Up => {
-                    snake[0][1] += 1;
-                }
-                Dir::Down => {
-                    snake[0][1] -= 1;
-                }
-                Dir::Left => {
-                    snake[0][0] -= 1;
-                }
-                Dir::Right => {
-                    snake[0][0] += 1;
-                }
-            }
+            add_inplace(&mut snake[0], dir);
 
-            for idx in 0..snake.len() - 1 {
-                let dx = snake[idx][0] - snake[idx + 1][0];
-                let dy = snake[idx][1] - snake[idx + 1][1];
-                let direction = [dx.signum(), dy.signum()];
-                let too_far = dx * dx + dy * dy > 2;
-                if too_far {
-                    match direction {
-                        [1, 0] => snake[idx + 1][0] += 1,
-                        [1, 1] => {
-                            snake[idx + 1][0] += 1;
-                            snake[idx + 1][1] += 1
-                        }
-                        [0, 1] => snake[idx + 1][1] += 1,
-                        [-1, 1] => {
-                            snake[idx + 1][0] -= 1;
-                            snake[idx + 1][1] += 1;
-                        }
-                        [-1, 0] => snake[idx + 1][0] -= 1,
-                        [-1, -1] => {
-                            snake[idx + 1][0] -= 1;
-                            snake[idx + 1][1] -= 1;
-                        }
-                        [0, -1] => snake[idx + 1][1] -= 1,
-                        [1, -1] => {
-                            snake[idx + 1][0] += 1;
-                            snake[idx + 1][1] -= 1;
-                        }
-                        _ => (),
-                    }
-                }
+            for idx in 1..snake.len() {
+                let previous = snake[idx - 1];
+                update_snake_mut(&mut snake[idx], previous);
             }
 
             positions_tail.insert(snake[9]);
         }
     }
+
     positions_tail.len()
 }
 
-fn parse_line(input: &str) -> IResult<&str, (Dir, i64)> {
-    let (rest, pair) = separated_pair(
-        alt((
-            value(Dir::Up, tag("U")),
-            value(Dir::Down, tag("D")),
-            value(Dir::Left, tag("L")),
-            value(Dir::Right, tag("R")),
-        )),
-        tag(" "),
-        i64,
-    )(input)?;
-    Ok((rest, pair))
+fn add_inplace([x1, y1]: &mut [i16; 2], [x2, y2]: [i16; 2]) {
+    *x1 += x2;
+    *y1 += y2;
 }
 
-#[derive(Debug, Clone, Copy)]
-enum Dir {
-    Up,
-    Down,
-    Left,
-    Right,
+fn get_deltas([x1, y1]: [i16; 2], [x2, y2]: [i16; 2]) -> [i16; 2] {
+    [x1 - x2, y1 - y2]
+}
+
+fn update_snake_mut(current: &mut [i16; 2], previous: [i16; 2]) {
+    let [dx, dy] = get_deltas(previous, *current);
+    if dx * dx + dy * dy > 2 {
+        let direction = [dx.signum(), dy.signum()];
+        add_inplace(current, direction);
+    }
+}
+
+fn parse_line(input: &str) -> IResult<&str, ([i16; 2], u8)> {
+    separated_pair(
+        alt((
+            value([0, 1], tag("U")),
+            value([0, -1], tag("D")),
+            value([-1, 0], tag("L")),
+            value([1, 0], tag("R")),
+        )),
+        tag(" "),
+        u8,
+    )(input)
 }
 
 #[cfg(test)]
 mod tests {
     use super::{part1, part2};
 
-    const TEST_INPUT: &str = "\
+    const TEST_INPUT_1: &str = "\
 R 4
 U 4
 L 3
@@ -166,6 +85,7 @@ R 4
 D 1
 L 5
 R 2";
+
     const TEST_INPUT_2: &str = "\
 R 5
 U 8
@@ -181,7 +101,7 @@ U 20";
     fn part1_works() {
         let output = 13;
 
-        assert_eq!(part1(TEST_INPUT), output);
+        assert_eq!(part1(TEST_INPUT_1), output);
     }
 
     #[test]
