59e6d19d5ac14995bb72ad7d05e62ecf4cbcc7bf
Miguel Licea <66740053+miglcesp01@users.noreply.github.com>
2022-12-11 01:35:40 -0500

    Refactoring solution day 11

--- LiceaMiguel/day_11.py
+++ LiceaMiguel/day_11.py
@@ -1,78 +1,56 @@
 from copy import deepcopy
 data = open('day11.in').read()
 
-class Monk():
-
+class Monk:
     def __init__(self, items, op, mod, true_target, false_target):
         self.items = items
         self.op = op
         self.mod = mod
         self.true_target = true_target
         self.false_target = false_target
-# monks = []
-# items = []
-# operations = []
-# mods = []
-# true_targets = []
-# false_targets = []
-# for simio in data.split("\n\n"):
-#     sim, item, op, mod, true_target, false_target = simio.split("\n")
-#     values = [int(c) for c in item.split(":")[1].split(",")]
-#     items.append(values)
-#     op = "".join(op.split()[-3:])
-#     operations.append(lambda old, op=op:eval(op))
-#     mods.append(int(mod.split()[-1]))
-#     true_targets.append(int(true_target.split()[-1]))
-#     false_targets.append(int(false_target.split()[-1]))
 
-items = []
-operations = []
-mods = []
-true_targets = []
-false_targets = []
+monks = []
 for simio in data.split("\n\n"):
     sim, item, op, mod, true_target, false_target = simio.split("\n")
-    values = [int(c) for c in item.split(":")[1].split(",")]
-    items.append(values)
     op = "".join(op.split()[-3:])
-    operations.append(lambda old, op=op:eval(op))
-    mods.append(int(mod.split()[-1]))
-    true_targets.append(int(true_target.split()[-1]))
-    false_targets.append(int(false_target.split()[-1]))
+    monk = Monk(items=[int(c) for c in item.split(":")[1].split(",")],
+        op = lambda old, op=op:eval(op),
+        mod = int(mod.split()[-1]),
+        true_target = int(true_target.split()[-1]),
+        false_target = int(false_target.split()[-1]))
+    monks.append(monk)
 
-init = deepcopy(items)
+mincm = 1
+for x in [monk.mod for monk in monks]:
+    mincm *= (mincm*x)
 
-values = [0 for _ in range(len(items))]
-items_copy = deepcopy(init)
+init = deepcopy(monks)
+values = [0]*len(monks)
 
 for _ in range(20):
-    for i in range(len(items_copy)):
-        for item in items_copy[i]:
+    for i,monk in enumerate(monks):
+        for item in monk.items:
             values[i]+=1
-            item = (operations[i](item)) // 3
-            if item % mods[i] ==0:
-                items_copy[true_targets[i]].append(item)
+            item = (monk.op(item)) // 3
+            if item % monk.mod == 0:
+                monks[monk.true_target].items.append(item)
             else:
-                items_copy[false_targets[i]].append(item)
-        items_copy[i] = []
+                monks[monk.false_target].items.append(item)
+        monk.items = []
 values.sort()
 print(values[-1] * values[-2])
 
-mincm = 1
-for i in mods:
-    mincm *= (mincm*i)
-
-items_copy = deepcopy(init)
-values = [0 for _ in range(len(items))]
+monks = init
+values = [0]*len(monks)
 for _ in range(10000):
-    for i in range(len(items_copy)):
-        for item in items_copy[i]:
+    for i, monk in enumerate(monks):
+        for item in monk.items:
             values[i]+=1
-            item = (operations[i](item)) % mincm
-            if item % mods[i] ==0:
-                items_copy[true_targets[i]].append(item)
+            item = (monk.op(item)) % mincm
+            if item % monk.mod == 0:
+                monks[monk.true_target].items.append(item)
             else:
-                items_copy[false_targets[i]].append(item)
-        items_copy[i] = []
+                monks[monk.false_target].items.append(item)
+        monk.items = []
 values.sort()
 print(values[-1] * values[-2])
