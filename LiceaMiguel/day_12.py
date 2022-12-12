e6b548a872bb35ce7718b7381ccd98c5b3a55d98
Miguel Licea <66740053+miglcesp01@users.noreply.github.com>
2022-12-12 01:24:25 -0500

    Refactoring solution day 12

--- LiceaMiguel/day_12.py
+++ LiceaMiguel/day_12.py
@@ -3,6 +3,7 @@ lines = [x for x in open('day12.in').read().split('\n')]
 
 num_rows = len(lines)
 num_columns = len(lines[0])
+directions = [(-1,0),(0,1),(1,0),(0,-1)]
 
 matrix = [[0 for _ in range(num_columns)] for _ in range(num_rows)]
 for i in range(num_rows):
@@ -14,48 +15,39 @@ for i in range(num_rows):
         else:
             matrix[i][j] = ord(lines[i][j])-ord('a')+1
 
-def find_shortest1():
-    doubly = deque()
+
+def find_start():
     for i in range(num_rows):
         for j in range(num_columns):
             if lines[i][j]=='S':
-                doubly.append(((i,j), 0))
+                return (i,j)
+    return (-1,-1)
 
-    S = set()
-    while doubly:
-        (i,j),d = doubly.popleft()
-        if (i,j) in S:
-            continue
-        S.add((i,j))
-        if lines[i][j]!='E':
-            for dr,dc in [(-1,0),(0,1),(1,0),(0,-1)]:
-                rr = i+dr
-                cc = j+dc
-                if 0<=rr<num_rows and 0<=cc<num_columns and matrix[rr][cc]<=1+matrix[i][j]:
-                    doubly.append(((rr,cc),d+1))
-        else: return d
-        
-
-def find_shortest2():
-    doubly = deque()
+def find_end():
     for i in range(num_rows):
         for j in range(num_columns):
             if matrix[i][j] == 1:
-                doubly.append(((i,j), 0))
+                return (i,j)
+    return (-1,-1)
 
-    S = set()
-    while doubly:
-        (i,j),d = doubly.popleft()
-        if (i,j) in S:
+def search(node):
+    queue = deque()
+    queue.append((node, 0))
+    visited = set()
+
+    while queue:
+        (i,j),dist = queue.popleft()
+        if (i,j) in visited:
             continue
-        S.add((i,j))
+        visited.add((i,j))
         if lines[i][j]!='E':
-            for dr,dc in [(-1,0),(0,1),(1,0),(0,-1)]:
-                rr = i+dr
-                cc = j+dc
-                if 0<=rr<num_rows and 0<=cc<num_columns and matrix[rr][cc]<=1+matrix[i][j]:
-                    doubly.append(((rr,cc),d+1))
-        else: return d
-        
-print(find_shortest1())
-print(find_shortest2())
+            for dir_row, dir_col in directions:
+                c_r = i+dir_row
+                c_c = j+dir_col
+                if 0<=c_r<num_rows and 0<=c_c<num_columns and matrix[c_r][c_c]<=1+matrix[i][j]:
+                    queue.append(((c_r,c_c),dist+1))
+        else:
+            return dist
+
+print(search(find_start()))
+print(search(find_end()))
