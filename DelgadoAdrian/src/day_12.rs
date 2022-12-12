3b712d067c369b086efb426406bfd076c5e5d20b
Adrian Delgado <git@adrianjdelgado.com>
2022-12-12 02:23:58 -0500

    More refactor

--- DelgadoAdrian/src/day_12.rs
+++ DelgadoAdrian/src/day_12.rs
@@ -1,7 +1,46 @@
 use bstr::ByteSlice;
 use ndarray::Array2;
-
 use petgraph::{algo::astar, prelude::*};
+
+fn create_array(rows: usize, cols: usize, input: Vec<u8>) -> Array2<i32> {
+    Array2::from_shape_vec(
+        (rows, cols),
+        input
+            .into_iter()
+            .map(|ch| {
+                i32::from(match ch {
+                    b'S' => b'a',
+                    b'E' => b'z',
+                    _ => ch,
+                })
+            })
+            .collect(),
+    )
+    .unwrap()
+}
+
+fn create_graph(arr: &Array2<i32>, rows: usize, cols: usize) -> DiGraphMap<[usize; 2], ()> {
+    let mut gr = DiGraphMap::new();
+    for (row, line) in arr.rows().into_iter().enumerate() {
+        for (col, ch) in line.into_iter().enumerate() {
+            gr.add_node([row, col]);
+
+            if row < rows - 1 && (arr[[row + 1, col]] - ch) <= 1 {
+                gr.add_edge([row, col], [row + 1, col], ());
+            }
+            if row > 0 && (arr[[row - 1, col]] - ch) <= 1 {
+                gr.add_edge([row, col], [row - 1, col], ());
+            }
+            if col > 0 && (arr[[row, col - 1]] - ch) <= 1 {
+                gr.add_edge([row, col], [row, col - 1], ());
+            }
+            if col < cols - 1 && (arr[[row, col + 1]] - ch) <= 1 {
+                gr.add_edge([row, col], [row, col + 1], ());
+            }
+        }
+    }
+    gr
+}
 
 pub fn part1(input: &str) -> i32 {
     let cols = input.find('\n').unwrap();
@@ -16,44 +55,11 @@ pub fn part1(input: &str) -> i32 {
         [idx / cols, idx % cols]
     };
 
-    let arr = Array2::<i32>::from_shape_vec(
-        (rows, cols),
-        input
-            .into_iter()
-            .map(|ch| {
-                i32::from(match ch {
-                    b'S' => b'a',
-                    b'E' => b'z',
-                    _ => ch,
-                })
-            })
-            .collect(),
-    )
-    .unwrap();
+    let arr = create_array(rows, cols, input);
 
-    let mut gr = DiGraphMap::new();
-    for (row, line) in arr.rows().into_iter().enumerate() {
-        for (col, ch) in line.into_iter().enumerate() {
-            gr.add_node([row, col]);
+    let gr = create_graph(&arr, rows, cols);
 
-            if row < rows - 1 && (arr[[row + 1, col]] - ch) <= 1 {
-                gr.add_edge([row, col], [row + 1, col], 1_i32);
-            }
-            if row > 0 && (arr[[row - 1, col]] - ch) <= 1 {
-                gr.add_edge([row, col], [row - 1, col], 1);
-            }
-            if col > 0 && (arr[[row, col - 1]] - ch) <= 1 {
-                gr.add_edge([row, col], [row, col - 1], 1);
-            }
-            if col < cols - 1 && (arr[[row, col + 1]] - ch) <= 1 {
-                gr.add_edge([row, col], [row, col + 1], 1);
-            }
-        }
-    }
-
-    astar(&gr, start, |f| f == end, |e| *e.weight(), |_| 0)
-        .unwrap()
-        .0
+    astar(&gr, start, |f| f == end, |_| 1, |_| 0).unwrap().0
 }
 
 pub fn part2(input: &str) -> i32 {
@@ -65,48 +71,17 @@ pub fn part2(input: &str) -> i32 {
         [idx / cols, idx % cols]
     };
 
-    let arr = Array2::<i32>::from_shape_vec(
-        (rows, cols),
-        input
-            .into_iter()
-            .map(|ch| {
-                i32::from(match ch {
-                    b'S' => b'a',
-                    b'E' => b'z',
-                    _ => ch,
-                })
-            })
-            .collect(),
-    )
-    .unwrap();
+    let arr = create_array(rows, cols, input);
 
-    let mut gr = DiGraphMap::new();
-    for (row, line) in arr.rows().into_iter().enumerate() {
-        for (col, ch) in line.into_iter().enumerate() {
-            gr.add_node([row, col]);
+    let gr = create_graph(&arr, rows, cols);
 
-            if row < rows - 1 && (arr[[row + 1, col]] - ch) <= 1 {
-                gr.add_edge([row, col], [row + 1, col], 1_i32);
-            }
-            if row > 0 && (arr[[row - 1, col]] - ch) <= 1 {
-                gr.add_edge([row, col], [row - 1, col], 1);
-            }
-            if col > 0 && (arr[[row, col - 1]] - ch) <= 1 {
-                gr.add_edge([row, col], [row, col - 1], 1);
-            }
-            if col < cols - 1 && (arr[[row, col + 1]] - ch) <= 1 {
-                gr.add_edge([row, col], [row, col + 1], 1);
-            }
-        }
-    }
-
-    let mut fewest_steps = 370;
+    let mut fewest_steps = i32::MAX;
 
     for (row, line) in arr.rows().into_iter().enumerate() {
         for (col, ch) in line.into_iter().enumerate() {
             if *ch == i32::from(b'a') {
                 let Some((steps, _)) =
-                    astar(&gr, [row, col], |f| f == end, |e| *e.weight(), |_| 0) else { continue; };
+                    astar(&gr, [row, col], |f| f == end, |_| 1, |_| 0) else { continue; };
 
                 fewest_steps = fewest_steps.min(steps);
             }
