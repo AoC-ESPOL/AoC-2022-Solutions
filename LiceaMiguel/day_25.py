d5099066543e535cdb01331d58e520ba1e4950b6
Miguel Licea <66740053+miglcesp01@users.noreply.github.com>
2022-12-25 01:34:59 -0500

    Solution day 25 small refactoring

--- LiceaMiguel/day_25.py
+++ LiceaMiguel/day_25.py
@@ -1,1 +1,20 @@
-lines = [line.strip() for line in open('day25.in')]
+lines = [line for line in open('day25.in').read().splitlines()]
+
+conversion = {'0':0,'1':1,'2':2,'-':-1,'=':2}
+
+def from_snafu(s):
+    n,mul = 0,1
+    for d in s[::-1]:
+        n+=conversion[d]*mul
+        mul *=5
+    return n
+    
+def to_snafu(num):
+    s1 = ''
+    while num > 0:
+        num, place = divmod(num + 2, 5)
+        s1 += '=-012'[place]
+    return s1
+
+sum = sum([from_snafu(line) for line in lines])
+print(to_snafu(sum))
